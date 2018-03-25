7f8985aa09a3b0fd56488b4109f5b279acc1fa48
Gabriel Ramos <chaspen27alt@gmail.com>
2018-03-25 02:10:47 -0400

    ma

--- main.py
+++ main.py
@@ -3,68 +3,74 @@ from pygame.locals import *
 from pygame import mixer
 import pyganim
 from pysndfx import AudioEffectsChain
-from PIL import Image, ImageEnhance
 import sys
 
 #init everything
-pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
+pygame.mixer.pre_init(frequency=44100, size=-16, channels=10, buffer=4096)
 pygame.init()
 pygame.mixer.init()
 pygame.font.init()
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-#display resolutions
-display_width =  1500
-display_height = 800
-#color definition
-black = (0,0,0)
-white = (256,256,256)
-red = (255,0,0)
-#general game setup
-gameDisplay = pygame.display.set_mode((display_width, display_height ))
-pygame.display.set_caption('FuzzForce')
-clock = pygame.time.Clock()
-#init AudioEffectsChain
-MenuFX = (
-    AudioEffectsChain()
-	.reverb()
-)
-#load media and play
-titlePic = pygame.image.load('images/title.png')
-backGroundPic = pygame.image.load('images/bg.png')
-pygame.mixer.music.load('music/menu-music.ogg')
-pygame.mixer.music.play()
 
-def QuitAct():
-    pygame.quit()
-    quit()
+def InitMain():
+	
+    #display resolutions
+    display_width =  1500
+    display_height = 800
+    #color definition
+    black = (0,0,0)
+    white = (256,256,256)
+    red = (255,0,0)
+    #general game setup
+    gameDisplay = pygame.display.set_mode((display_width, display_height ))
+    pygame.display.set_caption('FuzzForce')
+    clock = pygame.time.Clock()
+    #init AudioEffectsChain
+    MenuFX = (
+        AudioEffectsChain()
+	    .reverb()
+    )
+    #load media and play
+    titlePic = pygame.image.load('images/title.png')
+    backGroundPic = pygame.image.load('images/bg.png')
+    pygame.mixer.music.load('music/silent_col.ogg')
+    pygame.mixer.music.play()
 
-def title(x,y):                                              #title graphics
-	gameDisplay.blit(backGroundPic , (0,0))
-	gameDisplay.blit(titlePic, (x,y,))
+    def QuitAct():
+        pygame.quit()
+        quit()
 
-ConfirmStart = pygame.mixer.Sound('sfx/start-confirm.wav')
+    def title(x,y):                                              #title graphics
+	    gameDisplay.blit(backGroundPic , (0,0))
+	    gameDisplay.blit(titlePic, (x,y,))
 
-x = (display_width * 0)
-y = (display_height * 0)
+    ConfirmStart = pygame.mixer.Sound('sfx/start-confirm.wav')
 
-crashed = False
-while not crashed:                                            #Game loop starts here.
-	for event in pygame.event.get() :
-		if event.type == pygame.QUIT:
-			crashed = True                               #if crash, then close python and pygame processes.
-	if event.type == pygame.locals.KEYDOWN:                    #key registers start here
-		if event.key == pygame.K_SPACE:                        #when press space, stop (almost) everything and do the next thing
-			pygame.mixer.music.fadeout(3)
-			ConfirmStart.play(0)
-			titlePic.set_alpha(0)
-		if event.key == pygame.K_ESCAPE:
-			quit()
-			pygame.quit()
+    x = (display_width * 0)
+    y = (display_height * 0)
+
+    #make images scalable
+    GetBGSize = backGroundPic.get_rect()
+    GetLogoSize = titlePic.get_rect()
 
 
-	title(450,50)
-	pygame.display.update()
-	clock.tick(60)
+    crashed = False
+    while not crashed:                                            #Game loop starts here.
+	    for event in pygame.event.get() :
+		    if event.type == pygame.QUIT:
+			    crashed = True                               #if crash, then close python and pygame processes.
+	    if event.type == pygame.locals.KEYDOWN:                    #key registers start here
+		    if event.key == pygame.K_SPACE:                        #when press space, stop (almost) everything and do the next thing
+			    pygame.mixer.music.fadeout(3)
+			    ConfirmStart.play(0)
+			    titlePic.set_alpha(0)
+		    if event.key == pygame.K_ESCAPE:
+			    quit()
+			    pygame.quit()
 
-QuitAct()
+
+	    title(450,50)
+	    pygame.display.update()
+	    clock.tick(60)
+    QuitAct()
